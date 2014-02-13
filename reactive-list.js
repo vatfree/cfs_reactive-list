d374aa913b9c80e4bdc6a8ba01080fc5815f5180
Morten Henriksen <mh@gi2.dk>
2014-02-13 10:31:11 +0100

    lint

--- reactive-list.js
+++ reactive-list.js
@@ -1,8 +1,8 @@
 // #ReactiveList
 // Provides a simple reactive list interface
-_noopCallback = function() {};
+var _noopCallback = function() {};
 
-_nonReactive = {
+var _nonReactive = {
   changed: _noopCallback,
   depend: _noopCallback
 };
@@ -21,11 +21,11 @@ _nonReactive = {
   *   list.insert(3, { text: 'Hello id: 3' });
   *   list.update(2, { text: 'Updated 2'});
   *   list.remove(1);
-  *   
+  *
   *   list.forEach(function(value, key) {
   *     console.log('GOT: ' + value.text);
   *   }, true); // Set noneReactive = true, default behaviour is reactive
-  * 
+  *
   *   // Return from Template:
   *   Template.hello.list = function() {
   *     return list.fetch();
@@ -45,9 +45,9 @@ _nonReactive = {
   * ```
   * ###Object chain
   * ```
-  *                    first                               last
-  *   undefined -       obj       -       obj       -       obj       - undefined
-  *              (prev value next) (prev value next) (prev value next)
+  *                   first                               last
+  *  undefined -       obj       -       obj       -       obj       - undefined
+  *             (prev value next) (prev value next) (prev value next)
   * ```
   */
 ReactiveList = function(options) {
@@ -57,9 +57,9 @@ ReactiveList = function(options) {
   // Length
   self._length = 0;
   // First object in list
-  self.first;
+  self.first = null;
   // Last object in list
-  self.last;
+  self.last = null;
   // Set sort to options.sort or default to true (asc)
   self.sort = (options && options.sort || function(a, b) {
     return a.key < b.key;
@@ -69,7 +69,9 @@ ReactiveList = function(options) {
   self.isReactive = (options)? options.reactive !== false : true;
 
   // If lifo queue
-  if (options === true || options && options.sort === true) self.sort = function(a, b) { return a.key > b.key; };
+  if (options === true || options && options.sort === true) {
+    self.sort = function(a, b) { return a.key > b.key; };
+  }
 
   // Rig the dependencies
   self._listDeps = (self.isReactive)? new Deps.Dependency() : _nonReactive;
@@ -129,7 +131,8 @@ ReactiveList.prototype.update = function(key, value) {
 ReactiveList.prototype.insert = function(key, value) {
   var self = this;
   if (typeof self.lookup[key] !== 'undefined') {
-    throw new Error('Reactive list could not insert: key "' + key + '" allready found');
+    throw new Error('Reactive list could not insert: key "' + key +
+            '" allready found');
   }
   // Create the new item to insert into the list
   var newItem = { key: key, value: value };
@@ -146,14 +149,14 @@ ReactiveList.prototype.insert = function(key, value) {
     if (self.sort(newItem, current)) {
 
       // Insert self.lookup[key] before
-      if (typeof current.prev === 'undefined') self.first = newItem;
+      if (typeof current.prev === 'undefined') { self.first = newItem; }
 
       // Set the references in the inserted object
       newItem.prev = current.prev;
       newItem.next = current;
 
       // Update the two existing objects
-      if (current.prev) current.prev.next = newItem;
+      if (current.prev) { current.prev.next = newItem; }
       current.prev = newItem;
 
       // Mark the item as inserted - job's done
@@ -167,12 +170,12 @@ ReactiveList.prototype.insert = function(key, value) {
   if (!isInserted) {
     // We append it to the list
     newItem.prev = self.last;
-    if (self.last) self.last.next = newItem;
+    if (self.last) { self.last.next = newItem; }
 
     // Update the last pointing to newItem
     self.last = newItem;
     // Update first if we are appending to an empty list
-    if (self._length === 0) self.first = newItem;
+    if (self._length === 0) { self.first = newItem; }
   }
 
 
@@ -180,9 +183,9 @@ ReactiveList.prototype.insert = function(key, value) {
   self.lookup[key] = newItem;
   // Increase length
   self._length++;
-  self._lengthDeps.changed();  
+  self._lengthDeps.changed();
   // And invalidate the list
-  self._listDeps.changed();  
+  self._listDeps.changed();
 };
 
 /** @method ReactiveList.prototype.remove
@@ -196,7 +199,8 @@ ReactiveList.prototype.remove = function(key) {
   // Check that it exists
   if (typeof item === 'undefined') {
     return;
-    // throw new Error('ReactiveList cannot remove item, unknow key "' + key + '"');
+    // throw new Error('ReactiveList cannot remove item, unknow key "' + key +
+    //        '"');
   }
 
   // Rig the references
@@ -226,7 +230,7 @@ ReactiveList.prototype.remove = function(key) {
   delete self.lookup[key];
   // Decrease the length
   self._length--;
-  self._lengthDeps.changed();  
+  self._lengthDeps.changed();
   // Invalidate the list
   self._listDeps.changed();
 };
@@ -269,7 +273,7 @@ ReactiveList.prototype.forEach = function(f, noneReactive, reverse) {
     throw new Error('ReactiveList forEach requires a function');
   }
   // We allow this not to be reactive
-  if (!noneReactive) self._listDeps.depend();
+  if (!noneReactive) { self._listDeps.depend(); }
   // Set current to the first object
   var current = (reverse)?self.last: self.first;
   // Iterate over the list while its not empty
@@ -300,7 +304,7 @@ ReactiveList.prototype.fetch = function(noneReactive) {
   // Init the result buffer
   var result = [];
   // Iterate over the list items
-  self.forEach(function(value, key) {
+  self.forEach(function fetchCallback(value) {
     // Add the item value to the result
     result.push(value);
   }, noneReactive);
