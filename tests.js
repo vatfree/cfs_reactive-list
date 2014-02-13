ea09ef18e3425f112fdebf83248af512e7b97187
Morten Henriksen <mh@gi2.dk>
2014-02-13 10:30:26 +0100

    Add tests

--- tests.js
+++ tests.js
@@ -4,10 +4,84 @@ function equals(a, b) {
   return !!(JSON.stringify(a) === JSON.stringify(b));
 }
 
-Tinytest.add('ReactiveList - test', function(test) {
-  //TODO
+Tinytest.add('ReactiveList - definition', function(test) {
+  test.isTrue(typeof ReactiveList !== 'undefined', 'ReactiveList is undefined, make sure to add the reactive-list package');
 });
 
+
+// We do a small test of insert and remove
+Tinytest.addAsync('ReactiveList - basic insert and remove - test 1', function test1 (test, onComplete) {
+  Meteor.setTimeout(function() {
+    var list = new ReactiveList({
+      reactive: true
+    });
+
+    var testLength = 500;
+
+    for (var i = 0; i < testLength; i++)
+      list.insert(i, 'value' + i);
+
+    test.equal(list._length, testLength, 'List length is not as expected');
+
+    var order = 0;
+    list.forEach(function(value, key) {
+      test.equal(key, order, 'order is not as expected');
+      test.equal(value, 'value'+order, 'order is not as expected');
+      order++;
+    });
+
+    test.equal(order, testLength, 'forEach length is not as expected');
+
+    list.forEachReverse(function(value, key) {
+      order--;
+      test.equal(key, order, 'order is not as expected');
+      test.equal(value, 'value'+order, 'order is not as expected');
+    });
+
+    test.equal(order, 0, 'forEachReverse length is not as expected');
+
+    // Remove all items
+    for (var i = 0; i < testLength; i++)
+      list.remove(i);
+
+    test.equal(list._length, 0, 'List length is not as expected');
+
+    test.isUndefined(list.first, 'First should now be undefined');
+
+    test.isUndefined(list.last, 'Last should now be undefined');
+    onComplete();
+  }, 1000);  
+});
+
+
+// We test insert and remove on larger scale
+Tinytest.addAsync('ReactiveList - basic insert and reset - test 2', function test2 (test, onComplete) {
+  Meteor.setTimeout(function() {
+    var list = new ReactiveList({
+      reactive: true
+    });
+
+
+    var testLength = 500;
+
+    for (var i = 0; i < testLength; i++)
+      list.insert(i, 'value' + i);
+
+    test.equal(list._length, testLength, 'List length is not as expected');
+
+
+    // Remove all items
+    list.reset();
+
+    test.equal(list._length, 0, 'List length is not as expected');
+
+    test.isUndefined(list.first, 'First should now be undefined');
+
+    test.isUndefined(list.last, 'Last should now be undefined');
+
+    onComplete();
+  }, 5000);
+});
 
 //Test API:
 //test.isFalse(v, msg)
